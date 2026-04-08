b351ddbfba0481b1605af4de57c5d388d5f27514
pavel <mypmail@mail.ru>
2026-04-08 21:42:46 +0700

    Fix error 11

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,8 +14,6 @@ if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
 
-$services = ['db:3306', 'redis:6379', 'manticore:9306'];
-
 $skipWaiting = false;
 $argv = $_SERVER['argv'] ?? [];
 foreach ($argv as $key => $arg) {
@@ -26,6 +24,30 @@ foreach ($argv as $key => $arg) {
 }
 
 if (!$skipWaiting) {
+    $services = [];
+
+    // Parse DATABASE_URL
+    $databaseUrl = $_ENV['DATABASE_URL'] ?? $_SERVER['DATABASE_URL'] ?? '';
+    if ($databaseUrl && preg_match('/mysql:\/\/.*?@([^:\/?]+):(\d+)/i', $databaseUrl, $matches)) {
+        $services[] = $matches[1] . ':' . $matches[2];
+    }
+
+    // Parse REDIS_URL
+    $redisUrl = $_ENV['REDIS_URL'] ?? $_SERVER['REDIS_URL'] ?? '';
+    if ($redisUrl && preg_match('/redis:\/\/([^:\/?]+):(\d+)/', $redisUrl, $matches)) {
+        $services[] = $matches[1] . ':' . $matches[2];
+    }
+
+    // Parse Manticore
+    $manticoreHost = $_ENV['MANTICORE_HOST'] ?? $_SERVER['MANTICORE_HOST'] ?? '';
+    $manticorePort = $_ENV['MANTICORE_PORT'] ?? $_SERVER['MANTICORE_PORT'] ?? '9306';
+    if ($manticoreHost) {
+        $services[] = $manticoreHost . ':' . $manticorePort;
+    }
+
+    // Deduplicate
+    $services = array_unique($services);
+
     foreach ($services as $host) {
         $parts = explode(':', $host);
         $hostname = $parts[0];
